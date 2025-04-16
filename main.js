6f9896685fc988dd981c5a1c81d01df84dcf1c05
ollaariane <145364174+ollaariane@users.noreply.github.com>
2025-04-16 08:34:10 -0300

    uiui

--- main.js
+++ main.js
@@ -21,10 +21,8 @@ const tempoObjetivo2 = new Date("2025-10-31T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-15T00:00:00");
 const tempoObjetivo4 = new Date("2026-02-28T00:00:00");
 
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -37,6 +35,22 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if (tempoFinal > 0) {
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
 }
+
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
