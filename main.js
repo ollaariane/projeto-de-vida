2a852cfcabf0ee24c4c378b2d73e3ed7bd089890
ollaariane <145364174+ollaariane@users.noreply.github.com>
2025-05-07 11:10:20 -0300

    oo

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-05-30T00:00:00");
-const tempoObjetivo2 = new Date("2025-11-8T00:00:00");
+const tempoObjetivo2 = new Date("2025-11-08T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-20T00:00:00");
 const tempoObjetivo4 = new Date("2026-02-01T00:00:00");
 
